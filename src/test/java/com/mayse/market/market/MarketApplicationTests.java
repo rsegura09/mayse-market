183b8ecb6e108211826559ba0838c64f417a196b
Ricardo Segura <seguraricardo380@gmail.com>
2024-02-05 13:11:20 -0500

    Primer commit

---
+++ src/test/java/com/mayse/market/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayse.market.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
